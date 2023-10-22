21941ee942434ed52f44ff7f4f5b05de3e73df54
Oscar-roqsoft <oscar.nnoje@gmail.com>
2023-10-22 22:03:58 +0100

    set rate btn

--- pages/setRate/index.vue
+++ pages/setRate/index.vue
@@ -50,7 +50,7 @@
             <!-- end of warning -->
     
 
-            <nuxt-Link to="#" class="w-full my-6">
+            <nuxt-Link to="#" class="w-full my-10">
                 <button type="button" class="text-white w-full bg-gradient-to-r from-[#25FEB7] via-green-400 to-[#1DC7AC]
                  hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-green-300 font-medium  text-sm
                  rounded-md  p-3 text-center">Confirm</button>
